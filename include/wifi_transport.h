cda2545fb729f27f4ba79771878c6914567bb6a6
Payton Gagne <97078729+paytongagne@users.noreply.github.com>
2026-08-20 02:54:40 -0500

    Add captive-portal provisioning to WiFi transport

--- include/wifi_transport.h
+++ include/wifi_transport.h
@@ -2,6 +2,7 @@
 
 #include <Arduino.h>
 
+#include "device_config.h"
 #include "sensors.h"
 #include "system_state.h"
 
@@ -13,19 +14,28 @@ public:
     void loop(SensorManager &sensors, RuntimeState &runtime);
     void publishTelemetry(const String &packet);
     void queueCommand(const String &command);
+
     bool enabled() const;
     bool wifiConnected() const;
     bool mqttConnected() const;
+    bool configured() const;
     String deviceId() const;
     String ipAddress() const;
+    String mqttHost() const;
+    String setupAccessPointName() const;
 
 private:
+    bool runProvisioning();
+    void configureMqttClient();
     void serviceWifi();
     void serviceMqtt();
     void subscribeTopics();
     void processPendingCommand(SensorManager &sensors, RuntimeState &runtime);
     void publishResponseLines(const String &buffer);
 
+    DeviceConfigStore configStore_;
+    bool configurationLoaded_ = false;
+    bool provisioningSucceeded_ = false;
     unsigned long lastWifiAttemptMs_ = 0;
     unsigned long lastMqttAttemptMs_ = 0;
     String pendingCommand_;
