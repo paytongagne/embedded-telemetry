ac83dc201a7eada4b1821021dbe2ef0bbcdf0cfb
Payton Gagne <97078729+paytongagne@users.noreply.github.com>
2026-08-20 02:54:21 -0500

    Replace compile-time WiFi secrets with provisioning settings

--- include/wifi_config.h
+++ include/wifi_config.h
@@ -2,24 +2,10 @@
 
 #include <Arduino.h>
 
-#if __has_include("wifi_secrets.h")
-#include "wifi_secrets.h"
-#define WIFI_TELEMETRY_ENABLED 1
-#else
-#define WIFI_TELEMETRY_ENABLED 0
-namespace wifi_secrets {
-constexpr const char *SSID = "";
-constexpr const char *PASSWORD = "";
-constexpr const char *MQTT_HOST = "";
-constexpr uint16_t MQTT_PORT = 1883;
-constexpr const char *MQTT_USER = "";
-constexpr const char *MQTT_PASSWORD = "";
-constexpr const char *DEVICE_ID = "esp8285-node";
-}
-#endif
-
 namespace wifi_config {
 constexpr uint32_t RECONNECT_INTERVAL_MS = 5000;
+constexpr uint32_t PORTAL_TIMEOUT_SECONDS = 180;
 constexpr uint16_t MQTT_BUFFER_SIZE = 768;
 constexpr uint16_t MQTT_KEEPALIVE_SECONDS = 20;
+constexpr const char *SETUP_AP_PREFIX = "EmbeddedTelemetry-Setup";
 }
