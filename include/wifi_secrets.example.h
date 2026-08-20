22d237e67fe53f5d520f60e147840fe435800707
Payton Gagne <97078729+paytongagne@users.noreply.github.com>
2026-08-20 02:28:46 -0500

    Add WiFi credential template

---
+++ include/wifi_secrets.example.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace wifi_secrets {
+constexpr const char *SSID = "YOUR_WIFI_SSID";
+constexpr const char *PASSWORD = "YOUR_WIFI_PASSWORD";
+constexpr const char *MQTT_HOST = "192.168.1.10";
+constexpr uint16_t MQTT_PORT = 1883;
+constexpr const char *MQTT_USER = "";
+constexpr const char *MQTT_PASSWORD = "";
+constexpr const char *DEVICE_ID = "esp8285-node";
+}
