610ddf0feaae08f3efcaf694e670f335091f42e3
Payton Gagne <97078729+paytongagne@users.noreply.github.com>
2026-08-20 02:28:54 -0500

    Add WiFi MQTT transport interface

---
+++ include/wifi_transport.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <Arduino.h>
+
+#include "sensors.h"
+#include "system_state.h"
+
+class WifiTransport {
+public:
+    WifiTransport();
+
+    void begin();
+    void loop(SensorManager &sensors, RuntimeState &runtime);
+    void publishTelemetry(const String &packet);
+    bool enabled() const;
+    bool wifiConnected() const;
+    bool mqttConnected() const;
+    String deviceId() const;
+    String ipAddress() const;
+
+private:
+    void serviceWifi();
+    void serviceMqtt();
+    void subscribeTopics();
+    void processPendingCommand(SensorManager &sensors, RuntimeState &runtime);
+    void publishResponseLines(const String &buffer);
+
+    unsigned long lastWifiAttemptMs_ = 0;
+    unsigned long lastMqttAttemptMs_ = 0;
+    String pendingCommand_;
+};
